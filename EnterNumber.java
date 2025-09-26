787dc9ac651bc0263700a0fac17aae1416374fb2
Ahmad Salahuddin <160369477+Ah-ad7878@users.noreply.github.com>
2025-09-26 17:10:57 +0500

    Add files via upload

--- EnterNumber.java
+++ EnterNumber.java
@@ -7,7 +7,30 @@ public class EnterNumber {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
+        System.out.println();
 
+        int i = 1;
+        int zero = 0;
+        int positive = 0;
+        int negetive  = 0;
+        while (i<=number) {
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();
+
+
+            if (num == 0) {
+                zero++;
+            } else if (num>=0) {
+                positive++;
+            } else if (num<=0) {
+                negetive++;
+            }
+            i++;
+
+        }
+        System.out.println("strength of Zero number is "+zero);
+        System.out.println("strength of positive  number is "+positive);
+        System.out.println("strength of negative number is "+negetive);
 
     }
 }
